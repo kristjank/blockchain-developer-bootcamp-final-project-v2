3e7e78a0a3f04c8a83007640912009c4c09262ac
Kristjan Kosic <kristjan@protokol.com>
2022-08-08 16:43:48 +0000

    feat: Proposal creation

---
+++ scripts/propose.ts
@@ -0,0 +1,61 @@
+/* eslint-disable no-process-exit */
+// eslint-disable-next-line node/no-unpublished-import
+import { ethers, network } from "hardhat";
+import {
+  NEW_STORE_VALUE,
+  FUNC,
+  PROPOSAL_DESCRIPTION,
+  developmentChains,
+  VOTING_DELAY,
+  proposalsFile,
+} from "../helper-hardhat-config";
+import { moveBlocks } from "../utils/mode-blocks";
+import * as fs from "fs";
+
+export async function propose(args: any[], functionToCall: string, proposalDescription: string): Promise<void> {
+  const governor = await ethers.getContract("GovernorContract");
+  const box = await ethers.getContract("Box");
+
+  const encodedFunctionCall = box.interface.encodeFunctionData(functionToCall, args);
+
+  console.log(`Encoded Function Call ${encodedFunctionCall}`);
+
+  console.log(`Proposing ${functionToCall} on ${box.address} with args: ${args}`);
+  console.log(`Proposal description:  ${proposalDescription}`);
+
+  const proposeTx = await governor.propose([box.address], [0], [encodedFunctionCall], proposalDescription);
+
+  if (developmentChains.includes(network.name)) {
+    await moveBlocks(VOTING_DELAY + 1);
+  }
+
+  const proposeReceipt = await proposeTx.wait(1);
+  const proposalId = proposeReceipt.events[0].args.proposalId;
+  // console.log(proposeReceipt.events);
+  console.log(`Proposed with proposal ID:\n  ${proposalId}`);
+
+  const proposalState = await governor.state(proposalId);
+  const proposalSnapShot = await governor.proposalSnapshot(proposalId);
+  const proposalDeadline = await governor.proposalDeadline(proposalId);
+
+  // save the proposalId
+  const proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"));
+  proposals[network.config.chainId!.toString()].push(proposalId.toString());
+
+  fs.writeFileSync(proposalsFile, JSON.stringify(proposals));
+
+  // The state of the proposal. 1 is not passed. 0 is passed.
+  console.log(`Current Proposal State: ${proposalState}`);
+  // What block # the proposal was snapshot
+  console.log(`Current Proposal Snapshot: ${proposalSnapShot}`);
+  // The block number the proposal voting expires
+  console.log(`Current Proposal Deadline: ${proposalDeadline}`);
+}
+
+// await propose([77], "store");
+propose([NEW_STORE_VALUE], FUNC, PROPOSAL_DESCRIPTION)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
